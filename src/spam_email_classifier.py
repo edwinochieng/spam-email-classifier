45888870c96abeba08fbd8d86aa9d58f75ccd5fd
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-04 23:50:47 +0300

    train model

--- src/spam_email_classifier.py
+++ src/spam_email_classifier.py
@@ -9,3 +9,6 @@ X = data.iloc[:, :-1]  # Features
 y = data.iloc[:, -1]   # Target (class labels)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
