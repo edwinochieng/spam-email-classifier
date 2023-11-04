56625c30af42e839cf77fe310b9b39f2b6713926
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-04 22:59:21 +0300

    load dataset

--- src/spam_email_classifier.py
+++ src/spam_email_classifier.py
@@ -2,3 +2,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+data = pd.read_csv('../data/spambase.data', header=None)
