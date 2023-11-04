77b9f4ed190eb807c4f032724d12dbdfa37acbc2
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-04 23:49:08 +0300

    split dataset into train and testing sets

--- src/spam_email_classifier.py
+++ src/spam_email_classifier.py
@@ -8,3 +8,4 @@ data = pd.read_csv('../data/spambase.data', header=None)
 X = data.iloc[:, :-1]  # Features
 y = data.iloc[:, -1]   # Target (class labels)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
