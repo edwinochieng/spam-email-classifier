849790dfe1d3a3fc598b906be8fda0074affd22a
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-04 23:53:00 +0300

    make prediction on the test set

--- src/spam_email_classifier.py
+++ src/spam_email_classifier.py
@@ -12,3 +12,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
+
+#prediction
+y_pred = model.predict(X_test)
